e14c33240c987926009afc619a5abb7a83a60ef1
Markus Moenig <markus@moenig.io>
2025-03-21 15:00:57 +0700

    white background

--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -57,8 +57,8 @@ var relearn_searchindex = [
   },
   {
     "breadcrumb": "",
-    "content": "Coming soon",
-    "description": "Coming soon",
+    "content": "CHIPcade is a 16-bit retro arcade chip… or at least it thinks it is.\nProgram it on macOS and iPadOS to create your own classic-style games — no soldering iron required.\nFeatures Authentic* 16-bit arcade chip with 8 general-purpose registers and 4 user input registers. Powerful graphics co-processor with 8 layers and 255 hardware sprites. CHIPcade Script — a flexible, assembly-flavored scripting language. Built-in data sections, audio support, and customizable palette. Written in Swift and Metal for native performance on Apple hardware. Created games run on macOS, iOS, and tvOS. *Okay, it’s all software. But your game won’t know the difference.\nComing soon to the macOS App Store and iPad App Store.",
+    "description": "CHIPcade is a 16-bit retro arcade chip… or at least it thinks it is.\nProgram it on macOS and iPadOS to create your own classic-style games — no soldering iron required.\nFeatures Authentic* 16-bit arcade chip with 8 general-purpose registers and 4 user input registers. Powerful graphics co-processor with 8 layers and 255 hardware sprites. CHIPcade Script — a flexible, assembly-flavored scripting language. Built-in data sections, audio support, and customizable palette. Written in Swift and Metal for native performance on Apple hardware. Created games run on macOS, iOS, and tvOS. *Okay, it’s all software. But your game won’t know the difference.",
     "tags": [],
     "title": "CHIPcade",
     "uri": "/index.html"
